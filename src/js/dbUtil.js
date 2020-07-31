3ec8b2b46521a3674d27fe3eebaac48ec1dfaa4d
Chen Meng <c5meng@ucsd.edu>
2020-07-31 12:09:35 -0700

    Add function headers

--- src/js/dbUtil.js
+++ src/js/dbUtil.js
@@ -5,9 +5,96 @@ require("firebase/firestore");
 firebase.initializeApp(fbConfig);
 var db = firebase.firestore();
 
-db.collection("labels-q1").get().then((querySnapshot) => {
-    querySnapshot.forEach((doc) => {
-        console.log(`${doc.id} => ${doc.data()}`);
-    });
-});
 
+/*
+ * Adds the exercise to the db
+ * @param{string} name: name of the exercise, should be in the format of name_of_the_exercise
+ * @param{string} file: filename of the exercise page, e.g: "index.html"
+ * @param{string[]} labels_q1: array of labels as strings for question 1
+ * @param{string[]} labels_q2: array of labels as strings for question 2
+ * @param{string[]} labels_q3: array of labels as strings for question 3
+ */
+ function addExercise(name, file, labels_q1, labels_q2, labels_q3){
+	if(!Array.isArray(labels_q1) || !Array.isArray(labels_q2) || !Array.isArray(labels_q3)) { 
+		console.error("Invalid label input");
+		return;
+	}
+
+	// Add exersice to the db
+	let exerciseRef = db.collection("exercises").doc(name);
+	let status = exerciseRef.get().then(function(doc){
+		if(doc.exists) { throw "Exercise already exists"; }
+		else{ 
+			exerciseRef.set( {url: file} ); 
+			// Add exercise to q1 labels
+			const q1Ref = db.collection("labels_q1");
+			labels_q1.forEach(function(label) {
+				let labelRef = q1Ref.doc(label);
+				labelRef.get().then(function(doc){
+					let field = new Object();
+					field[name] = true;
+
+					if(doc.exists){
+						labelRef.set(field, {merge: true}); 
+					}
+					else{ 
+						labelRef.set(field); 
+					}
+				}).catch(function(error){ 
+					throw error;
+				});
+			});
+
+			// Add exercise to q2 labels
+			const q2Ref = db.collection("labels_q2");
+			labels_q2.forEach(function(label) {
+				let labelRef = q2Ref.doc(label);
+				labelRef.get().then(function(doc){
+					let field = new Object();
+					field[name] = true;
+
+					if(doc.exists){
+						labelRef.set(field, {merge: true}); 
+					}
+					else{ 
+						labelRef.set(field); 
+					}
+				}).catch(function(error){ 
+					throw error;
+				});
+			});
+
+			// Add exercise to q3 labels
+			const q3Ref = db.collection("labels_q3");
+			labels_q3.forEach(function(label) {
+				let labelRef = q3Ref.doc(label);
+				labelRef.get().then(function(doc){
+					let field = new Object();
+					field[name] = true;
+
+					if(doc.exists){
+						labelRef.set(field, {merge: true}); 
+					}
+					else{ 
+						labelRef.set(field); 
+					}
+				}).catch(function(error){ 
+					throw error;
+				});
+			});
+		}
+	}).catch(function(err){ 
+		console.error(err); 
+	});
+}
+
+function queryExercise(label, question){
+	switch(question) {
+		case "q1": 
+		case "q2":
+		case "q3":
+	}
+}
+
+
+module.exports = { addExercise }
