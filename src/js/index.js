68da699eb12ea1a93b6142ed12299c5eb66a4b34
Chen Meng <c5meng@ucsd.edu>
2020-08-06 21:26:45 -0700

    Add getLabels method

--- src/js/index.js
+++ src/js/index.js
@@ -13,13 +13,16 @@ app.post('/addExercise', async (req, res) => {
   const file = req.body.file;
   const labels_q1 = req.body.labels_q1;
   const labels_q2 = req.body.labels_q2;
-  const labesl_q3 = req.body.labesl_q3;
+  const labels_q3 = req.body.labels_q3;
 
-  const promise = dbUtil.addExercise(name, file, labels_q1, labels_q2, labesl_q3);
+  const promise = dbUtil.addExercise(name, file, labels_q1, labels_q2, labels_q3);
   promise.then(function (value) {
+    res.status(200);
     res.send("Added Exercise successfully!");
   }, function (value) {
     console.log("error in adding exercise to db");
+    res.status(500);
+    res.send("Failed to add exercise");
   });
 });
 
@@ -29,16 +32,32 @@ app.post('/queryExercise', async (req, res) => {
 
   const promise = dbUtil.queryExercise(label, question);
   promise.then(function (value) {
+    console.log(value);
     const exercises_arr = value.map((ex) => ({ [ex]: ex.url }));
     console.log("Queried exercises succesfully!");
+    res.status(200);
     res.send(exercises_arr);
   }, function (value) {
     console.log("error in querying db for exercises");
+    res.status(500);
+    res.send("Failed to add exercise");
   });
 
 });
 
-// app.post('/getLabels') --- TO-DO
+ app.post('/getLabels', async(req, res) => {
+  const question = req.body.question;
+  const promise = dbUtil.getLabels(question);
+  promise.then(function (value){
+    console.log(value);
+    res.status(200);
+    res.send(value);
+  }).catch(function(err){
+    console.log("error getting labels");
+    res.status(500);
+    res.send("Failed to get labels");
+  });
+ });
 
 app.listen(8080, () => {
   console.log('Example app listening on port 8080!')
