5c7e02a3a7e57f7a8ebbc94533307a5e7a565b53
Shufan Li <jacklishufan@berkeley.edu>
2021-04-01 21:31:27 -0700

    update

--- lib/screens/emoji_page.dart
+++ lib/screens/emoji_page.dart
@@ -11,6 +11,7 @@ import 'package:retreatapp/constants.dart';
 import 'package:retreatapp/screens/input_page.dart';
 import 'package:retreatapp/screens/mood_details.dart';
 import 'package:retreatapp/screens/results_page.dart';
+import 'package:easy_web_view/easy_web_view.dart';
 
 import '../models/brain.dart';
 
@@ -19,6 +20,91 @@ class EmojiPage extends StatefulWidget {
   _EmojiPageState createState() => _EmojiPageState();
 }
 
+const disclaimer = '''
+<h2><span style="font-size: 1.5em;">Disclaimer</span><br>
+</h2>
+
+<p>Re:treat aims to bridge the gap between professional mental and psychiatric care and college students’ daily
+    emotional distress, but <strong>the website is NOT meant to substitute any advice, diagnosis, or treatment plans
+        provided by medical professionals</strong>. If you suspect that you are facing mental-health related problems,
+    you are strongly encouraged to seek professional help.</p>
+
+<p>In regard to anonymity, Re:treat <strong>is fully anonymous and is NOT recording any identifiable data </strong>(e.g.
+    your email or name) of our users, although we caution you against disclosing too personal information on the sharing
+    platform since we have also published the website to Berkeley students only. We will also monitor posts that show
+    potential warning signs of self-harm, suicidality, or health emergency, and <strong>reply with a referral
+        message</strong> suggesting further interventions and help seeking.</p>
+
+<p>Information found on this website should not be taken as medical advice or reproduced in any form without the
+    permission of Re:treat team. For enquiries and feedbacks, please feel free to email <a
+            href="mailto:hello@re-treat.app">hello@re-treat.app</a></p>
+
+<p>Thank you for all the support and welcome to Re:treat!</p>
+
+<p><strong>List of available mental health resource:</strong></p>
+
+<p><strong>CAPS</strong></p>
+
+<p>UC Berkeley Counseling and Psychological Services (CAPS) provide counseling for academic, personal and career
+    concerns. Psychiatry services for when medication can help with counseling.</p>
+
+<p><span style="box-sizing: border-box; -webkit-print-color-adjust: exact; border-bottom: 0.05em solid;">https://uhs.berkeley.edu/caps</span>
+</p>
+
+<p><strong>SoS</strong></p>
+
+<p>Social Service Counseling provide counseling for specialized concerns such as chronic medical illnesses, pregnancy,
+    eating disorders and nutrition, violence, and alcohol or other drugs</p>
+
+<p><span style="box-sizing: border-box; -webkit-print-color-adjust: exact; border-bottom: 0.05em solid;">https://uhs.berkeley.edu/socialservices</span>
+</p>
+
+<p><strong>TAO</strong></p>
+
+<p>TAO connect is an online library of interactive programs that help you learn life skills and bounce back from
+    disappointment to achieve specific goals. TAO offers educational modules, assessments, practice tools and logs, and
+    a mindfulness library.</p>
+
+<p><span style="box-sizing: border-box; -webkit-print-color-adjust: exact; border-bottom: 0.05em solid;">https://uhs.berkeley.edu/tao</span>
+</p>
+
+<p><strong>SSPC</strong></p>
+
+<p>Student to Student Peer Counseling (SSPC) is a group of student counselors providing free, one-on-one, confidential,
+    walk-in services to UC Berkeley students.</p>
+
+<p><span style="box-sizing: border-box; -webkit-print-color-adjust: exact; border-bottom: 0.05em solid;">https://sspc.berkeley.edu</span>
+</p>
+
+<p><strong>Mental Health Support at Undocumented Student Program</strong></p>
+
+<p>mental health support program provides a confidential space for undocumented students to be seen by a licensed
+    therapist who will collaborate with them on an individual wellness plan. Support varies by each student’s need
+    including: one-time drop-in consultation, crisis management, on-going sessions, and connection to other campus
+    resources.</p>
+
+<p><span style="box-sizing: border-box; -webkit-print-color-adjust: exact; border-bottom: 0.05em solid;">https://undocu.berkeley.edu/academic-support/mental-health-support/</span>
+</p>
+
+<p><strong>Berkeley Free Clinic Peer Counseling</strong></p>
+
+<p>The Berkeley Free Clinic provides free, confidential peer counseling for individuals 18 years of age and over.
+    Services are provided by volunteers trained in active listening. During COVID-19, we’re offering peer counseling by
+    phone.</p>
+
+<p><a href="https://www.berkeleyfreeclinic.org/peer-counseling">https://www.berkeleyfreeclinic.org/peer-counseling</a>
+</p>
+
+<p><strong>National Suicide Prevention Lifeline</strong></p>
+
+<p>If you&apos;re having suicidal thoughts, call <strong>1-800-273-TALK (8255)</strong> to talk to a skilled, trained
+    counselor at a crisis center in your area at any time. If you are located outside the United States, call your local
+    emergency line immediately.</p>
+
+<p><br>
+</p>
+
+''';
 class EmojiCard extends StatefulWidget {
   Widget _picture = SVG_LOVE;
   String _tag = 'love';
@@ -167,7 +253,19 @@ class _EmojiPageState extends State<EmojiPage> {
               children: emojiRenderList
 
             )
-              ,
+              , SliverToBoxAdapter(child:EasyWebView(
+                src: disclaimer,
+                isHtml: true,
+                // Use Html syntax
+                isMarkdown: false,
+                // Use markdown syntax
+                convertToWidgets: true,
+                // Try to convert to flutter widgets
+                width: 100,
+                height: 1000,
+                onLoaded: () {},
+                key: const ValueKey("disclaimer")
+            ),)
             ]
         )
     );
@@ -261,6 +359,7 @@ class _EmojiPageState extends State<EmojiPage> {
                         ),
                       )
                      ,  ]),
+
               Container(
                 padding: EdgeInsets.all(8),
               margin: EdgeInsets.all (30),
