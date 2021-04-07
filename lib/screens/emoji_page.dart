b5546200b3d31cd5d5271f85faf4195c5610c0e4
Chen Meng <c5meng@ucsd.edu>
2021-04-06 21:24:54 -0700

    Add disclaimer

--- lib/screens/emoji_page.dart
+++ lib/screens/emoji_page.dart
@@ -11,6 +11,7 @@ import 'package:retreatapp/constants.dart';
 import 'package:retreatapp/screens/input_page.dart';
 import 'package:retreatapp/screens/mood_details.dart';
 import 'package:retreatapp/screens/results_page.dart';
+import 'package:easy_web_view/easy_web_view.dart';
 
 import '../models/brain.dart';
 
@@ -19,6 +20,91 @@ class EmojiPage extends StatefulWidget {
   _EmojiPageState createState() => _EmojiPageState();
 }
 
+const disclaimer = '''
+<h2><span style="font-size: 1.5em;">Disclaimer</span><br>
+</h2>
+
+<p>Re:treat aims to bridge the gap between professional mental and psychiatric care and college students’ daily
+    emotional distress, but <strong>the website is NOT meant to substitute any advice, diagnosis, or treatment plans
+        provided by medical professionals</strong>. If you suspect that you are facing mental-health related problems,
+    you are strongly encouraged to seek professional help.</p>
+
+<p>In regard to anonymity, Re:treat <strong>is fully anonymous and is NOT recording any identifiable data </strong>(e.g.
+    your email or name) of our users, although we caution you against disclosing too personal information on the sharing
+    platform since we have also published the website to Berkeley students only. We will also monitor posts that show
+    potential warning signs of self-harm, suicidality, or health emergency, and <strong>reply with a referral
+        message</strong> suggesting further interventions and help seeking.</p>
+
+<p>Information found on this website should not be taken as medical advice or reproduced in any form without the
+    permission of Re:treat team. For enquiries and feedbacks, please feel free to email <a
+            href="mailto:hello@re-treat.app">hello@re-treat.app</a></p>
+
+<p>Thank you for all the support and welcome to Re:treat!</p>
+
+<p><strong>List of available mental health resource:</strong></p>
+
+<p><strong>CAPS</strong></p>
+
+<p>UC Berkeley Counseling and Psychological Services (CAPS) provide counseling for academic, personal and career
+    concerns. Psychiatry services for when medication can help with counseling.</p>
+
+<p><span style="box-sizing: border-box; -webkit-print-color-adjust: exact; border-bottom: 0.05em solid;">https://uhs.berkeley.edu/caps</span>
+</p>
+
+<p><strong>SoS</strong></p>
+
+<p>Social Service Counseling provide counseling for specialized concerns such as chronic medical illnesses, pregnancy,
+    eating disorders and nutrition, violence, and alcohol or other drugs</p>
+
+<p><span style="box-sizing: border-box; -webkit-print-color-adjust: exact; border-bottom: 0.05em solid;">https://uhs.berkeley.edu/socialservices</span>
+</p>
+
+<p><strong>TAO</strong></p>
+
+<p>TAO connect is an online library of interactive programs that help you learn life skills and bounce back from
+    disappointment to achieve specific goals. TAO offers educational modules, assessments, practice tools and logs, and
+    a mindfulness library.</p>
+
+<p><span style="box-sizing: border-box; -webkit-print-color-adjust: exact; border-bottom: 0.05em solid;">https://uhs.berkeley.edu/tao</span>
+</p>
+
+<p><strong>SSPC</strong></p>
+
+<p>Student to Student Peer Counseling (SSPC) is a group of student counselors providing free, one-on-one, confidential,
+    walk-in services to UC Berkeley students.</p>
+
+<p><span style="box-sizing: border-box; -webkit-print-color-adjust: exact; border-bottom: 0.05em solid;">https://sspc.berkeley.edu</span>
+</p>
+
+<p><strong>Mental Health Support at Undocumented Student Program</strong></p>
+
+<p>mental health support program provides a confidential space for undocumented students to be seen by a licensed
+    therapist who will collaborate with them on an individual wellness plan. Support varies by each student’s need
+    including: one-time drop-in consultation, crisis management, on-going sessions, and connection to other campus
+    resources.</p>
+
+<p><span style="box-sizing: border-box; -webkit-print-color-adjust: exact; border-bottom: 0.05em solid;">https://undocu.berkeley.edu/academic-support/mental-health-support/</span>
+</p>
+
+<p><strong>Berkeley Free Clinic Peer Counseling</strong></p>
+
+<p>The Berkeley Free Clinic provides free, confidential peer counseling for individuals 18 years of age and over.
+    Services are provided by volunteers trained in active listening. During COVID-19, we’re offering peer counseling by
+    phone.</p>
+
+<p><a href="https://www.berkeleyfreeclinic.org/peer-counseling">https://www.berkeleyfreeclinic.org/peer-counseling</a>
+</p>
+
+<p><strong>National Suicide Prevention Lifeline</strong></p>
+
+<p>If you&apos;re having suicidal thoughts, call <strong>1-800-273-TALK (8255)</strong> to talk to a skilled, trained
+    counselor at a crisis center in your area at any time. If you are located outside the United States, call your local
+    emergency line immediately.</p>
+
+<p><br>
+</p>
+
+''';
 class EmojiCard extends StatefulWidget {
   Widget _picture = SVG_LOVE;
   String _tag = 'love';
@@ -86,53 +172,53 @@ class _EmojiCardState extends State<EmojiCard> {
           hoverColor: color,
         ),
         child:InkWell(
-        onTap: () {
-          logVisit("emoji-detail+"+widget._tag,{
-            "from":"MoodBoard",
-            "emoji": widget._tag
-          });
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) =>
-                  moodDetails(moodId: widget._tag),
-            ),
-          );
-        },
-      onHover: (isHovering) {
-        if (isHovering) {
-          setState(() {
-            _scaled=true;
-          });;
-        } else {
-          setState(() {
-            _scaled = false;
-          });
-        }
-      },
-        child: Container(
-            decoration: BoxDecoration(
-              color: Colors.white,
-              shape: BoxShape.circle,
-              boxShadow: [
-                BoxShadow(
-                    blurRadius: 10, color: kLightGrayColor, spreadRadius: 2)
-              ],
-            ),
-            padding: const EdgeInsets.all(40),
+            onTap: () {
+              logVisit("emoji-detail+"+widget._tag,{
+                "from":"MoodBoard",
+                "emoji": widget._tag
+              });
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) =>
+                      moodDetails(moodId: widget._tag),
+                ),
+              );
+            },
+            onHover: (isHovering) {
+              if (isHovering) {
+                setState(() {
+                  _scaled=true;
+                });;
+              } else {
+                setState(() {
+                  _scaled = false;
+                });
+              }
+            },
+            child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  shape: BoxShape.circle,
+                  boxShadow: [
+                    BoxShadow(
+                        blurRadius: 10, color: kLightGrayColor, spreadRadius: 2)
+                  ],
+                ),
+                padding: const EdgeInsets.all(40),
 
-            child: CircleAvatar(
-              radius: 60*getScale(),
-              //半径，图形放大是radius的2倍
-              //foregroundColor：子视图中文字的颜色
-              //不设置backgroundColor和foregroundColor，CircleAvatar默认背景色是灰色
-              //不设置backgroundColor，backgroundColor的颜色随foregroundColor变化而变化，但与foregroundColor色值不同
-              //同时设置backgroundColor、backgroundImage，能直接看到的是backgroundImage
-              foregroundColor: Colors.white,
-              child: widget._picture,
-              backgroundColor: Colors.white,
+                child: CircleAvatar(
+                  radius: 60*getScale(),
+                  //半径，图形放大是radius的2倍
+                  //foregroundColor：子视图中文字的颜色
+                  //不设置backgroundColor和foregroundColor，CircleAvatar默认背景色是灰色
+                  //不设置backgroundColor，backgroundColor的颜色随foregroundColor变化而变化，但与foregroundColor色值不同
+                  //同时设置backgroundColor、backgroundImage，能直接看到的是backgroundImage
+                  foregroundColor: Colors.white,
+                  child: widget._picture,
+                  backgroundColor: Colors.white,
 //      backgroundImage: NetworkImage(imageNetwork),
-            ))));
+                ))));
   }
   Widget build(BuildContext context) {
     return getCard();
@@ -162,12 +248,24 @@ class _EmojiPageState extends State<EmojiPage> {
         flex: 1,
         child: CustomScrollView(
             slivers: [SliverGrid.extent(
-              maxCrossAxisExtent: 300,
+                maxCrossAxisExtent: 300,
                 mainAxisSpacing: 10,
-              children: emojiRenderList
+                children: emojiRenderList
 
             )
-              ,
+              , SliverToBoxAdapter(child:EasyWebView(
+                  src: disclaimer,
+                  isHtml: true,
+                  // Use Html syntax
+                  isMarkdown: false,
+                  // Use markdown syntax
+                  convertToWidgets: true,
+                  // Try to convert to flutter widgets
+                  width: 100,
+                  height: 1000,
+                  onLoaded: () {},
+                  key: const ValueKey("disclaimer")
+              ),)
             ]
         )
     );
@@ -183,26 +281,26 @@ class _EmojiPageState extends State<EmojiPage> {
 //        centerTitle: false,
 //      ),
         body: Center(child: Consumer<Brain>(
-            builder: (context, brain, child) {
-              return Column(
-                children: <Widget>[
-                  Stack(
-                      children:[Container(
-              decoration: BoxDecoration(
-                  image: DecorationImage(
-                  image: AssetImage("images/bg.jpg"),
-              fit: BoxFit.cover,
-              ),
-              ),
+          builder: (context, brain, child) {
+            return Column(
+              children: <Widget>[
+                Stack(
+                    children:[Container(
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage("images/bg.jpg"),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
 //                  color:Colors.teal[40],
 //                    padding: const EdgeInsets.all(8),
-                        width: MediaQuery
-                            .of(context)
-                            .size
-                            .width ,
-                    height:150,
-                    child:const Text(""),
-                  )
+                      width: MediaQuery
+                          .of(context)
+                          .size
+                          .width ,
+                      height:150,
+                      child:const Text(""),
+                    )
                       , Positioned.fill(
                         child: Align(
                             alignment: Alignment.center,
@@ -234,6 +332,7 @@ class _EmojiPageState extends State<EmojiPage> {
                                         alignment: Alignment(1, 0),
                                         child: OutlinedButton(
                                           onPressed: () {
+                                            logVisit('ExerciseInputPage',{"from":"MoodBoard"});
                                             Navigator.push(
                                               context,
                                               MaterialPageRoute(
@@ -243,15 +342,15 @@ class _EmojiPageState extends State<EmojiPage> {
                                             );
                                           },
                                           child: Container(
-                                            padding: EdgeInsets.all(10),
+                                              padding: EdgeInsets.all(10),
                                               child:Text('Take me to exercises')),
                                           style: ButtonStyle(
-                                              backgroundColor: MaterialStateProperty
-                                                  .all(Colors.white),
-                                              foregroundColor: MaterialStateProperty
-                                                  .all(kEmojiTag),
-                                              elevation: MaterialStateProperty
-                                                  .all(10),
+                                            backgroundColor: MaterialStateProperty
+                                                .all(Colors.white),
+                                            foregroundColor: MaterialStateProperty
+                                                .all(kEmojiTag),
+                                            elevation: MaterialStateProperty
+                                                .all(10),
                                           ),
                                         ),
                                       )
@@ -259,71 +358,73 @@ class _EmojiPageState extends State<EmojiPage> {
                                 ))
                         ),
                       )
-                     ,  ]),
-              Container(
-                padding: EdgeInsets.all(8),
-              margin: EdgeInsets.all (30),
-              decoration: BoxDecoration(
-              color: Colors.white,
-                borderRadius: BorderRadius.circular(10),
-              boxShadow: [
-              BoxShadow(blurRadius: 10,
-              color: kLightGrayColor,
-              spreadRadius: 2)
-              ],
-              ),
-                child: RichText(
-                  text: TextSpan(
-                    text: ' ',
-                    style: TextStyle(
-                      fontSize: 18,
-                        fontFamily: 'OpenSans'
-                      //fontWeight: FontWeight.w600,
-                    ),
-                    children: <TextSpan>[
-                      TextSpan(text: 'Find your ', ),
-                      TextSpan(text: 'emotion community ', style:
-                        TextStyle(
-                        color: Colors.orange
-                      )),
-                      TextSpan(text: 'and read '),
-                      TextSpan(text: 'top-hit stories ',style:
-                      TextStyle(
-                          color: Colors.orange,
-                      )),
-                      TextSpan(text: 'happening this week on UC Berkeley campus'),
+                      ,  ]),
+
+                Container(
+                  padding: EdgeInsets.all(8),
+                  margin: EdgeInsets.all (30),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(blurRadius: 10,
+                          color: kLightGrayColor,
+                          spreadRadius: 2)
                     ],
                   ),
+                  child: RichText(
+                    text: TextSpan(
+                      text: ' ',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontFamily: 'OpenSans',
+                        color:Colors.black,
+                        //fontWeight: FontWeight.w600,
+                      ),
+                      children: <TextSpan>[
+                        TextSpan(text: 'Find your ', ),
+                        TextSpan(text: 'emotion community ', style:
+                        TextStyle(
+                            color: Colors.orange
+                        )),
+                        TextSpan(text: 'and read '),
+                        TextSpan(text: 'top-hit stories ',style:
+                        TextStyle(
+                          color: Colors.orange,
+                        )),
+                        TextSpan(text: 'happening this week on UC Berkeley campus'),
+                      ],
+                    ),
 
-                ),
-              )
-              ,
+                  ),
+                )
+                ,
 
-              Expanded(child:Container(
-                width: MediaQuery
-                    .of(context)
-                    .size
-                    .width * 0.85,
+                Expanded(child:Container(
+                    width: MediaQuery
+                        .of(context)
+                        .size
+                        .width * 0.85,
 //                      color: Colors.teal[200],
 //                  constraints: BoxConstraints.expand(),
                     child:(
                         Column(
-                      children:<Widget>[
-                        Container(
-                          padding: const EdgeInsets.all(0),
-                          child: const Text(''),
+                            children:<Widget>[
+                              Container(
+                                padding: const EdgeInsets.all(0),
+                                child: const Text(''),
 
-                        ),
-                        getEmojiList()
-                      ]
-                    ))
-                  ))
+                              ),
+                              getEmojiList()
+                            ]
+                        ))
+                ))
 
 
-                ],
-              )
-              ;
-            },
+              ],
+            )
+            ;
+          },
 
         )
         )
